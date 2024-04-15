b23380278510683a1d1af65ce6cb5396b57ae853
Jason Whitlow <jason.whitlow45@gmail.com>
2024-04-14 19:28:23 -0700

    insert_stocks.sql

---
+++ sql_files/insert_daily_metrics.sql
@@ -0,0 +1,6 @@
+-- Insert Stocks
+insert INTO Stock (symbol, company_name, stock_exchange) VALUES ('AAPL', 'Apple Inc', 'NASDAQ');
+insert INTO Stock (symbol, company_name, stock_exchange) VALUES ('COIN', 'Coinbase Global Inc', 'NASDAQ');
+insert INTO Stock (symbol, company_name, stock_exchange) VALUES ('GOOGL', 'Alphabet Inc', 'NASDAQ');
+insert INTO Stock (symbol, company_name, stock_exchange) VALUES ('MSFT', 'Microsoft Corp', 'NASDAQ');
+insert INTO Stock (symbol, company_name, stock_exchange) VALUES ('PLTR', 'Palantir Technologies Inc', 'NASDAQ');
