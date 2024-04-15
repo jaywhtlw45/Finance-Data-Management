7b12872b593c44b38434ee36b5e62210189717e6
Jason Whitlow <jason.whitlow45@gmail.com>
2024-04-14 19:06:48 -0700

    modify queries

--- sql_files/example_queries.sql
+++ sql_files/example_queries.sql
@@ -13,15 +13,22 @@
 -- What is the current value of all Hudson's investment account?
 
 -- What is the largest position in Hudson's investment account?
--- position = stock with the largest evaluation at the current time. 
--- 1. Check the most recent stock date and price of stock, then multiply by number of shares in the account.
+-- position = value of a stock at the current time multiplied by the quantity of that stock in the account. 
+-- 1. Check the most recent price of the stock, then multiply by number of shares in the account.
 -- 2. Do this for all stocks and choose the maximum.
+
+-- What percent of the account is each stock for Hudson's investment account?
+-- 1. Calculate the position of for each stock in the account. 
+-- 2. Use the sum of all positions to calculate the percentage of each stock.
+
+-- What is the total worth of Hudson's investment account (cash position and investment)?
 
 -- Can also include querries about daily metrics------------------------------
 
 -- Which stock has the highest market cap?
 
 -- Which stock had the highest movement on the open over a given 2 week span?
+-- This question may be too difficult to answer.
 
 
 
